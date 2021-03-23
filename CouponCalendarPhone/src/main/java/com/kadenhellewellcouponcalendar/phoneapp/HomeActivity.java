6476e9ef362001f9b7b76b60fba72bb88866ac5a
Kaden <kdex95@yahoo.com>
2021-03-22 21:32:01 -0600

    Added top app bar and side navigation menu (just a template, really ). Other small fixes, like spelling.

--- CouponCalendarPhone/src/main/java/com/kadenhellewellcouponcalendar/phoneapp/HomeActivity.java
+++ CouponCalendarPhone/src/main/java/com/kadenhellewellcouponcalendar/phoneapp/HomeActivity.java
@@ -1,8 +1,12 @@
 package com.kadenhellewellcouponcalendar.phoneapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        MaterialToolbar toolbar = findViewById(R.id.topAppBar);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        toolbar.setNavigationOnClickListener(view -> {
+            drawer.open();
+        });
+
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(menuItem -> {
+            menuItem.setChecked(true);
+            //TODO switch statement on menuItem.getItemId()
+
+            drawer.close();
+            return true;
+        });
     }
 }
