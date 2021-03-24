48d51573014c86a82fc7cd2bd24924ee54f09a81
Kaden <kdex95@yahoo.com>
2021-03-23 22:27:00 -0600

    implemented signing out

--- CouponCalendarPhone/src/main/java/com/kadenhellewellcouponcalendar/phoneapp/HomeActivity.java
+++ CouponCalendarPhone/src/main/java/com/kadenhellewellcouponcalendar/phoneapp/HomeActivity.java
@@ -2,11 +2,15 @@ package com.kadenhellewellcouponcalendar.phoneapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationView;
+import com.kadenhellewellcouponcalendar.api.viewmodels.UserViewModel;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -15,6 +19,7 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        UserViewModel userViewModel = new ViewModelProvider(this).get(UserViewModel.class);
         MaterialToolbar toolbar = findViewById(R.id.topAppBar);
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         toolbar.setNavigationOnClickListener(view -> {
@@ -25,7 +30,18 @@ public class HomeActivity extends AppCompatActivity {
         navigationView.setNavigationItemSelectedListener(menuItem -> {
             menuItem.setChecked(true);
             //TODO switch statement on menuItem.getItemId()
-
+            switch (menuItem.getItemId())
+            {
+                case R.id.home_item:
+                    //TODO redirect to home fragment
+                    break;
+                case R.id.sign_out_item:
+                    userViewModel.signOut();
+                    Intent intent = new Intent(this, SignInUpActivity.class);
+                    startActivity(intent);
+                    finish();
+                    break;
+            }
             drawer.close();
             return true;
         });
