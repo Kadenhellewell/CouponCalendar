76a7818f2817791933d1b4e0628dfef0785a7585
Kaden <kdex95@yahoo.com>
2021-04-12 16:52:25 -0600

    Got the search stuff to not crash the app

--- CouponCalendarPhone/src/main/java/com/kadenhellewellcouponcalendar/phoneapp/HomeActivity.java
+++ CouponCalendarPhone/src/main/java/com/kadenhellewellcouponcalendar/phoneapp/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.kadenhellewellcouponcalendar.phoneapp;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -10,24 +11,41 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
+import com.kadenhellewellcouponcalendar.api.viewmodels.CouponViewModel;
 import com.kadenhellewellcouponcalendar.api.viewmodels.UserViewModel;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.search.MapboxSearchSdk;
+import com.mapbox.search.ResponseInfo;
+import com.mapbox.search.SearchEngine;
+import com.mapbox.search.SearchRequestTask;
+import com.mapbox.search.SearchSelectionCallback;
+import com.mapbox.search.result.SearchResult;
+import com.mapbox.search.result.SearchSuggestion;
+
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
     public MaterialToolbar toolbar;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
-        setContentView(R.layout.activity_home);
 
+        //Set up various mapbox things
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
+
+
+        setContentView(R.layout.activity_home);
         UserViewModel userViewModel = new ViewModelProvider(this).get(UserViewModel.class);
+        CouponViewModel couponViewModel = new ViewModelProvider(this).get(CouponViewModel.class);
+        couponViewModel.setUser(userViewModel.getUser());
         toolbar = findViewById(R.id.topAppBar);
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         toolbar.setNavigationOnClickListener(view -> {
--- CouponCalendarPhone/src/main/java/com/kadenhellewellcouponcalendar/phoneapp/MapFragment.java
+++ CouponCalendarPhone/src/main/java/com/kadenhellewellcouponcalendar/phoneapp/MapFragment.java
@@ -14,6 +14,7 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -41,8 +42,19 @@ import com.mapbox.mapboxsdk.style.layers.LineLayer;
 import com.mapbox.mapboxsdk.style.layers.Property;
 import com.mapbox.mapboxsdk.style.layers.PropertyFactory;
 import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
+import com.mapbox.search.MapboxSearchSdk;
+import com.mapbox.search.ResponseInfo;
+import com.mapbox.search.SearchEngine;
+import com.mapbox.search.SearchOptions;
+import com.mapbox.search.SearchRequestTask;
+import com.mapbox.search.SearchSelectionCallback;
+import com.mapbox.search.result.SearchResult;
+import com.mapbox.search.result.SearchSuggestion;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class MapFragment extends Fragment {
@@ -51,18 +63,67 @@ public class MapFragment extends Fragment {
     ArrayList<Point> points = new ArrayList<>(); //TODO have this be an array of coupon locations
     HomeActivity activity;
 
+    //Various mapbox things
+    public SearchEngine searchEngine;
+    public SearchRequestTask searchRequestTask;
+
+    public final SearchSelectionCallback searchCallback = new SearchSelectionCallback() {
+
+        @Override
+        public void onSuggestions(
+                @NonNull List<? extends SearchSuggestion> suggestions,
+                @NonNull ResponseInfo responseInfo
+        ) {
+            if (suggestions.isEmpty()) {
+                Log.i("SearchApiExample", "No suggestions found");
+            } else {
+                Log.i("SearchApiExample", "Search suggestions: " + suggestions + "\nSelecting first...");
+                searchRequestTask = searchEngine.select(suggestions.get(0), this);
+            }
+        }
+
+        @Override
+        public void onResult(
+                @NonNull SearchSuggestion searchSuggestion,
+                @NonNull SearchResult result,
+                @NonNull ResponseInfo responseInfo
+        ) {
+            Log.i("SearchApiExample", "Search result: " + result);
+        }
+
+        @Override
+        public void onCategoryResult(
+                @NonNull SearchSuggestion searchSuggestion,
+                @NonNull List<? extends SearchResult> results,
+                @NonNull ResponseInfo responseInfo
+        ) {
+            Log.i("SearchApiExample", "Category search results: " + results);
+        }
+
+        @Override
+        public void onError(@NonNull Exception e) {
+            Log.i("SearchApiExample", "Search error: ", e);
+        }
+    };
+
+
 
     public MapFragment() {
         super(R.layout.fragment_map);
-
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState){
         super.onViewCreated(view, savedInstanceState);
-        activity = (HomeActivity)getActivity();
 
-        //TODO this where map stuff goes
+        activity = (HomeActivity)getActivity();
+        searchEngine = MapboxSearchSdk.createSearchEngine();
+
+        final SearchOptions options = new SearchOptions.Builder()
+                .limit(5)
+                .build();
+
+        searchRequestTask = searchEngine.search("Paris Eiffel Tower", options, searchCallback);
 
         mapView = view.findViewById(R.id.map);
         mapView.onCreate(savedInstanceState);
@@ -73,9 +134,91 @@ public class MapFragment extends Fragment {
                 mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
                     @Override
                     public void onStyleLoaded(@NonNull Style style) {
+                        LocationManager manager = (LocationManager)activity.getSystemService(Context.LOCATION_SERVICE);
                         if (activity.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                             activity.requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1);
                         }
+
+                        MarkerViewManager markerViewManager = new MarkerViewManager(mapView, mapboxMap);
+
+                        GeoJsonSource source = new GeoJsonSource("points",
+                                FeatureCollection.fromFeatures(new Feature[]{
+                                        Feature.fromGeometry(LineString.fromLngLats(points))
+                                })
+                        );
+
+                        searchRequestTask = searchEngine.search("Paris Eiffel Tower", options, searchCallback);
+
+                        style.addSource(source);
+
+                        style.addLayer(new LineLayer("line-layer", "points").withProperties(
+                                PropertyFactory.lineDasharray(new Float[] {0.01f, 2f}),
+                                PropertyFactory.lineCap(Property.LINE_CAP_ROUND),
+                                PropertyFactory.lineJoin(Property.LINE_JOIN_ROUND),
+                                PropertyFactory.lineWidth(5f),
+                                PropertyFactory.lineColor(Color.parseColor("#e55e5e"))
+                        ));
+
+                        mapboxMap.addOnMapClickListener(new MapboxMap.OnMapClickListener() {
+                            @Override
+                            public boolean onMapClick(@NonNull LatLng point) {
+
+                                return true;
+                            }
+                        });
+
+                        LocationComponent locationComponent = mapboxMap.getLocationComponent();
+
+                        locationComponent.activateLocationComponent(
+                                LocationComponentActivationOptions.builder(activity, style)
+                                        .useSpecializedLocationLayer(true)
+                                        .build()
+                        );
+
+                        locationComponent.setLocationComponentEnabled(true);
+
+                        locationComponent.setCameraMode(CameraMode.TRACKING_GPS_NORTH, new OnLocationCameraTransitionListener() {
+                            @Override
+                            public void onLocationCameraTransitionFinished(int cameraMode) {
+                                locationComponent.zoomWhileTracking(17, 100);
+                            }
+
+                            @Override
+                            public void onLocationCameraTransitionCanceled(int cameraMode) {
+
+                            }
+                        });
+
+                        LocationComponentOptions options = locationComponent.getLocationComponentOptions().toBuilder()
+                                .trackingGesturesManagement(true)
+                                .trackingInitialMoveThreshold(500)
+                                .build();
+
+                        locationComponent.applyStyle(options);
+
+
+
+                        locationComponent.addOnCameraTrackingChangedListener(new OnCameraTrackingChangedListener() {
+                            @Override
+                            public void onCameraTrackingDismissed() {
+                                System.out.println("DONE TRACKING");
+                                new Thread(() -> {
+                                    try {
+                                        Thread.sleep(5000);
+                                    } catch (InterruptedException e) {
+                                        e.printStackTrace();
+                                    }
+                                    activity.runOnUiThread(() -> {
+                                        locationComponent.setCameraMode(CameraMode.TRACKING_GPS_NORTH);
+                                    });
+                                }).start();
+                            }
+
+                            @Override
+                            public void onCameraTrackingChanged(int currentMode) {
+                                System.out.println("TRACKING CHANGED " + currentMode);
+                            }
+                        });
                     }
                 });
             }
